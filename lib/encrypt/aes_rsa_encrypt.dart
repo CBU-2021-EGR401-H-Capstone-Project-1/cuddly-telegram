ad5291136b354d481d888382a0df65c4f2197047
LDeForrest97 <46612302+LDeForrest97@users.noreply.github.com>
2022-03-04 12:57:02 -0800

    Update aes_rsa_encrypt.dart

--- lib/encrypt/aes_rsa_encrypt.dart
+++ lib/encrypt/aes_rsa_encrypt.dart
@@ -1,6 +1,7 @@
 import 'package:cuddly_telegram/encrypt/read_write.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:crypton/crypton.dart' as crypton;
+import 'package:flutter/foundation.dart';
 
 class AppEncrypt {
   static final _key = encrypt.Key.fromSecureRandom(32);
@@ -22,12 +23,12 @@ class AppEncrypt {
   }
 
   //Creates an instance the AES encryption/decryption Algorithm. Mostly used to share data between both encrypter/decrypt classes. Encrypter Data Type fund in the encrypt.dart package
-  dynamic getEncrypter(dynamic init, dynamic valToEncrypt, iv) {
+  dynamic getAESEncryptInitializer(dynamic init, dynamic valToEncrypt, iv) {
     dynamic encrypted = init.encrypt(valToEncrypt, iv: iv);
     return encrypted;
   }
 
-  dynamic getDecrypter(dynamic init, dynamic valToEncrypt, iv) {
+  dynamic getAESDecryptInitializer(dynamic init, dynamic valToEncrypt, iv) {
     dynamic decrypted = init.decrypt(valToEncrypt, iv: iv);
     return decrypted;
   }
@@ -41,11 +42,11 @@ class AppEncrypt {
 }
 
 main() async {
-  //await Reader_WriterEncrypt('/Users/ladmin/OneDrive - California Baptist University/Capstone_Current/cuddly_telegram/lib/encrypt/Testing.dart');
-  await Reader_WriterDecrypt('/Users/ladmin/OneDrive - California Baptist University/Capstone_Current/cuddly_telegram/lib/encrypt/Testing.dart', 'MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDXwVlvSvzKfg/F6Sq9eRBzn2E3+HUHV5EwiC5bIffVzlM40i0i1G/hheLktTXXeJ1ExnyDzAhWptPjGqfCj49VZKyO6Fv+R3SCdpQAO9K+PhxU6mMDn7k2Xd0zOghl4aR00YX/zKaOT26smXo49KNqBkFT3gLSuA6zDkWeXFQkC8BqbUrBOnS2Uo/mFocov6kZ/fxRgSziaXObxxX5XUAjYFjE139bW0cQOZcz1bTuIdu9rMDpinYVnwwgMsOOnpie/IEqF9QmGLuIpqkoRtEoBzzvKm8oxxqS53OL9Rx+GscgsiFLnKI0Kj3Mfh/s2W/fqeJx7GMGdCjT/lXXcvqNAgMBAAECggEBAIAeg8JP8D86jplCtauf6YIPYca/1pbOjBwashRR3n7rL4YtYLovFmUAgVS4uslCCXnet+/3x/fUxzw0BuzFHSNjTgjquD4OJoSWb5qX67Qm4+IU8UkknvJG2OTjkDsXu/XWudDFsR4O0zTRSr/pneSG5GIuEtUYkToIvpSkVR61d+GoJPupuGfUOj9UziDWHV/INyhbvTO9g/TIT6ilYLSBV0nST4BA1OgL05tPYoS4T66TBG31KWLZfI+DQf7m0u8ele8qtxNeNaFOqVXMvYIrfoknJqGhyKmv0T+05kfZ5duIwAAngX4+0DQEGxYsMh98c1aVRnGaSbBkqQavjMECgYEA/RfZ7EQfCP2t73kxTJ1g9GLEGSrHLYB6EHGAiLKeT6/3gyv0tdlYZO5n4xwn79aOzsKmc6B4ysrQeWzSx2Fwv4C32xZpYPOCXWodtB4jvLiSqAX+qZ9luyYyDKy5JMs0uYrdqkpoYIx59jzjKHVmWt+GyndCRgAGvNgCbQ+Af5ECgYEA2ju3ch54S+tKUL27voDlYMXzW3eydJTZ+kZ8wKUg02BaOcs2BIroxGgOKJVNBTHru+msbo0RzhO/wae57/CudSoSiwDHPcNc6h4csMg8sxLPNBwbd0dEiExBczc1dJDlL8nfNBEKdiZhfZuWrwN4GxL/0rLIE5K7yOpObcgKxT0CgYEA5uomCCy27CsX0RAP7MkCNrcOF7Ax/c/kjrAgB5iAXFA+UU1/fZMJ1Ty3SDzeKzLlJLvvxjs5Oj0FLGQfg43hore8B4ZRKwXBgZ1Z8IY3MO5t0CN4ECbAWJomu+2zFmDYgBi02bP9u2kvtXwj/Tvv4SVkA8H/k4Jeo1mqDPtB0CECgYADXdZ3oTaz3R5ZXoPt7WkcdBFMAO201LMEJGA6TJHUEPFzYMolicsLdJt7TspQYTPEw1cQoYZ7ylwz7ZV1uR3H2u1MTafFMSVWyz4gEAa3sHuXzzMxRN0uyRZdY6WkdbPDZ/bUg8rIhdENMgBCFOieak2d/3oMqz/QWqT5FpPr9QKBgQCAr4LYoOKCdigdb8QyiSD2C9soZZMEPIYs0MOfQT7FYlO+3tBrJbVzdcOPWM/G+HbThpr1b+7BqY4EM6vCt2SjFTcK0VR0lQxXv8PYukk720lhtvRIqYzH7lUiSpfFPqVCCT90RuPYKbFmVT7/YSdtcgZqIcJrDAVCywPI4fnPgA==', 'KmjbAFfddhIo8i7MtQJxrn2PS/FX0PFrE1h4GjFk0jI=');
+  //await Reader_WriterEncrypt('/Users/levi/Library/CloudStorage/OneDrive-CaliforniaBaptistUniversity/Capstone_Current/cuddly_telegram/lib/encrypt/Testing.dart');
+  await decryptReadWrite('/Users/levi/Library/CloudStorage/OneDrive-CaliforniaBaptistUniversity/Capstone_Current/cuddly_telegram/lib/encrypt/Testing.dart', 'M10s1UPEF74iAI2s3NfLZGLEq7ACAChrZzPuU4CKeQc=', 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCX2UUE2o+3ycK86D1L0WT06LIVLgKZLHDTxexeyLV+ueh7gLR8Zx3iU2Zv6xXIB/LCzne/uKxB0K8EG+iPFxWPszQx1Rt59+uvcNp3ORhw+O8tQ7C9EQfcHAVHcB3p5Y2E7W138KOREqLl9onTtkuVLwiaRNQiHkKcunni3qo0karEHawlxVoaOisocdytgT1NipjVKLd7WT6LlHY8xnPfAOlPg2HvoyCp/z9Kkxtbp7O4GX03ZTdqppW5tRHlz716be0poWFAKBhL6OgfKSrc3fI+KwEfSniKkaQ6QICDYEsK7cbJ2ODaE5xkNFPyTrHUh0TKXgW1TaiEGW6VPsj7AgMBAAECggEAbGcdUKO/KdONhFK/P5sS2YwtZdVc4YDKY0TOw7PBbeDGSTTOpGjw+pvTa16GRDD4a33+Gw55Wtrhtqs90LkIGXgzcMfUP7RvuuVz8Nz/x9bO2J+UUsUPuWD/m8o8cLgB2b2uAov2GSEWzdi8+DkRqETVfVxZnKnBoCW30fBnzfJ1gvdsj/WSpKGTpLWuCaNi771hWVwmnxkWiBwL5iaOgKpFJdOAQ+xgyju8q3ZHOh43Xiyg4stUPVCdeahpNuEaXMircZEP4WFI9hlL6RsqIM7lo0em8KTkv8/n8Y52cyAg/uO/GjOz5XpQvvrZvsh9B0uTm9u0G4GgWTHuHkDKcQKBgQD9GTj0WfzKwg0hz/4q4cC0Ynx79ILanF4c+2sPM3nSwsHt3ibEYWTZ+oATxvg2uesG2qpRgIlLStKrHLMXw7mzlW6ctEt77+LqH4P/ESOTI8puqGKQivI5xEeHeqMVRgZ8rrfwIqOU+Y8xr4TAAbVjz2QrvtBBgbCjMdKEfGEDnwKBgQCZlufUFpTFswyQ2H5ypR/4L1n3gGn6+DfY7MisX3csgx8BqH3GCeD36UlEwQlPBZO9F4tBvFRh1BgwM4CqISd2+rmw25w13ac1uk6vJpsbL8vJyUwymBwzrI7RC3CYMpRMUxnuyKLRXqDbjCobkF8yM/jDfgxAPSsfSSNA5kfdJQKBgQC7lrwt75x7s0QEcaKSqewoRm65eMqbuRXQKVB6Xc7HNW6DHQpit0UGgrH29pv2A+p8lAl0iu43jeeCx6y9ymWTAwiOwNrJq0zl9iHhJRTW88oQJmGXfER4KFBTy8Of+tzIAL94DlRsEGPfkZW0sg99QLOf8LduNe9zpXPAmmdVRQKBgG8vakYDztFLq9YTk35FAV62UKe2Y4JWPH+h8ieuuGQVy9V0dxBtSFnPnMXUBHwbKndh2uLMhj2Hv7btIcCHXb1pBhH8+RmZixl+9MUg6noE4L8EJVAfA5N44K5+XJUhUG/sXMKaphtxKHum/TiVDAUY0IYc8ptybIwqcwXhbratAoGBAOpeEYMGh3Jjc2wpdbrLmK1UxOTFGLpG6WGeIrWqgIW4it0QIDg3YZyMCv07iOuWCNz/Zc7x5m84P6d/2001RbBk7/uWQ/xLDcqq4SEUcaofr09lFaL1ZArGTuHR/CcimFFJ+ZamG63F1AjCVroAwn9TBhQIvybrrSfJNjXB62EK' );
 }
 
-Future<dynamic> Reader_WriterEncrypt(dynamic filePath) async {
+Future<dynamic> encryptReadWrite(dynamic filePath) async {
   var path = ReadWrite().userFile(setTakeUserPath(filePath));
   dynamic display;
   var holdFileContent = await ReadWrite().testReading(path);
@@ -69,12 +70,18 @@ Future<dynamic> Reader_WriterEncrypt(dynamic filePath) async {
   final keyInst = AppEncrypt().getKey.base64;
   final rsaPvtInst = AppEncrypt().getRSAPubKey().privateKey.toString();
   await ReadWrite().writeCounter(path, display3);
-  print(display3.toString().length);
-  print(keyInst);
-  print(rsaPvtInst);
+  if (kDebugMode) {
+    print(display3.toString().length);
+  }
+  if (kDebugMode) {
+    print(keyInst);
+  }
+  if (kDebugMode) {
+    print(rsaPvtInst);
+  }
 }
 
-Future<dynamic> Reader_WriterDecrypt(dynamic filePath, final rsaPvtKey, final aesKey) async {
+Future<dynamic> decryptReadWrite(dynamic filePath, final aesKey, final rsaPvtKey) async {
   var path = ReadWrite().userFile(setTakeUserPath(filePath));
   var holdFileContent = await ReadWrite().testReading(path);
   final holdFileLength = holdFileContent.toString().length;
@@ -94,8 +101,10 @@ Future<dynamic> Reader_WriterDecrypt(dynamic filePath, final rsaPvtKey, final ae
     display = "";
     y += 344;
   }
-  dynamic display2 = decryptAES(rsaSectorDecrypt, "$aesKey");
-  await ReadWrite().writeCounter(path, "$display2");
+  dynamic display2 = decryptAES(
+      rsaSectorDecrypt, "$aesKey");
+  await ReadWrite().writeCounter(
+      path, "$display2");
 }
 
 //File Path Setter
@@ -106,20 +115,21 @@ dynamic setTakeUserPath(dynamic enterFileLocation) {
 //Encrypts the string to ECB_AES Encryption. Takes in a String value, and spits out an Integer.
 dynamic encryptAES(dynamic valToEncrypt) {
   var key = AppEncrypt().getKey;
-  // var iv = AppEncrypt().getIV; unused
-  dynamic init = AppEncrypt().getAESInit(key);
+  dynamic init = AppEncrypt().getAESInit(
+      key);
   dynamic encrypted =
-      AppEncrypt().getEncrypter(init, valToEncrypt, AppEncrypt().getIV);
+      AppEncrypt().getAESEncryptInitializer(
+          init, valToEncrypt, AppEncrypt().getIV);
   final cipherText = encrypted.base64;
   return cipherText;
 }
 
 //Decrypts a String encrypted by RSA with a PKCS1 signing. Takes the String from the encrypt_RSA method and Decrypts it.
 dynamic decryptRSA(dynamic rsaEncrypt, final pvtKey) {
-  final rsaPvtKey = crypton.RSAPrivateKey.fromString("$pvtKey");
+  final rsaPvtKey = crypton.RSAPrivateKey.fromString(
+      "$pvtKey");
   final decrypt = rsaPvtKey.decrypt(rsaEncrypt);
   return decrypt;
-  //RSA Decryption of file is working!!!
 }
 
 //Encrypts the hash from encrypt_AES. RSA PKCS1 signing can't encrypt anything higher than 2048 bits or 256 bytes. Dynamic method outputs a dynamic var that will hold a String.
@@ -134,8 +144,10 @@ dynamic decryptAES(dynamic encrypted, var decryptKey) {
   var iv = AppEncrypt().getIV;
   var key1 = encrypt.Key.fromBase64(
       decryptKey); // THIS LINE IS IMPORTANT FOR AES DECRYPTION OF A FILE!!!!!!!!!!!!
-  dynamic init = AppEncrypt().getAESInit(key1);
-  dynamic decrypt = init.decrypt64(encrypted, iv: iv);
+  dynamic init = AppEncrypt().getAESInit(
+      key1);
+  dynamic decrypt = init.decrypt64(
+      encrypted, iv: iv);
   return decrypt;
 
   //AES Decryption of a file is working!!!!!
